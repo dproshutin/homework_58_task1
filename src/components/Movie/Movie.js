8e5dccf614006cefa3fb7c41f197159fb099e57b
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-10-20 15:19:05 +0600

    Initial setup of containers and components is done.

---
+++ src/components/Movie/Movie.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Movie.css';
+
+class Movie extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Movie;
---
+++ src/containers/WatchList/WatchList.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './WatchList.css';
+
+class WatchList extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default WatchList;
