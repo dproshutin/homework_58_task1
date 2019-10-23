b00c014f1c88886658e3d7c1480549524e332855
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-10-23 15:12:14 +0600

    Readme.md file is modified to contain the technical specification.

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import InputField from '../UI/InputField/InputField';
-import './Movie.css';
 import Button from "../UI/Button/Button";
 
 class Movie extends Component {
--- src/components/UI/InputField/InputField.js
+++ src/components/UI/InputField/InputField.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './InputField.css';
 
 const InputField = props => {
     return (
